4cfa9ffe4af66caca293895d526af523bf51b579
Nihal Vijoy(UST <287920@ust.com>
2024-11-05 15:54:59 +0530

    Created more views

--- server/server/api/urls.py
+++ server/server/api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views.main.main_views import WeddingViewSet
+from .views.main.main_views import BudgetItemViewSet, GuestViewSet, VendorViewSet, WeddingVendorViewSet, WeddingViewSet,ChecklistViewSet
 
 
 router = DefaultRouter()
-router.register(r'weddings', WeddingViewSet)
+router.register(r'wedding', WeddingViewSet)
+router.register(r'guest', GuestViewSet)
+router.register(r'checklist',ChecklistViewSet)
+router.register(r'budgetitem', BudgetItemViewSet)
+router.register(r'vendor',VendorViewSet)
+router.register(r'weddingvendor',WeddingVendorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- server/server/api/views/main/main_views.py
+++ server/server/api/views/main/main_views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
-
-from .serializers import WeddingSerializer
-from ...models import Wedding
+from rest_framework.permissions import IsAuthenticated
+from .serializers import WeddingSerializer,GuestSerializer,ChecklistSerializer,BudgetItemSerializer,VendorSerializer,WeddingVendorSerializer
+from ...models import Wedding,Guest,Checklist,BudgetItem,Vendor,WeddingVendor
 
 
 class WeddingViewSet(viewsets.ModelViewSet):
@@ -11,3 +11,47 @@ class WeddingViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(planner = self.request.user)
 
+class GuestViewSet(viewsets.ModelViewSet):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        wedding_id = self.request.data.get('wedding')
+        wedding = Wedding.objects.get(id=wedding_id)
+        serializer.save(wedding=wedding)
+
+class ChecklistViewSet(viewsets.ModelViewSet):
+    queryset = Checklist.objects.all()
+    serializer_class = ChecklistSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        wedding_id = self.request.data.get('wedding')
+        wedding = Wedding.objects.get(id=wedding_id)
+        serializer.save(wedding=wedding)
+
+class BudgetItemViewSet(viewsets.ModelViewSet):
+    queryset = BudgetItem.objects.all()
+    serializer_class = BudgetItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        wedding_id = self.request.data.get('wedding')
+        wedding = Wedding.objects.get(id=wedding_id)
+        serializer.save(wedding=wedding)
+
+class VendorViewSet(viewsets.ModelViewSet):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+    permission_classes = [IsAuthenticated]
+
+class WeddingVendorViewSet(viewsets.ModelViewSet):
+    queryset = WeddingVendor.objects.all()
+    serializer_class = WeddingVendorSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        wedding_id = self.request.data.get('wedding')
+        wedding = Wedding.objects.get(id=wedding_id)
+        serializer.save(wedding=wedding)
